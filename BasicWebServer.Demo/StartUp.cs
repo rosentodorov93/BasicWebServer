8e5d9845dc7026b90b5b1417c044301bf81159d2
Rosen Todorov <rosen.vt55@gmail.com>
2022-09-14 12:07:37 +0300

    Implemented simple downloading web content as a file function using TextFileResponse

--- BasicWebServer.Demo/StartUp.cs
+++ BasicWebServer.Demo/StartUp.cs
@@ -11,14 +11,26 @@
            Age: <input type='number' name ='Age'/>
         <input type='submit' value ='Save' />
         </form>";
+        private const string DownloadForm = @"<form action='/Content' method='POST'>
+   <input type='submit' value ='Download Sites Content' /> 
+</form>";
+        private const string FileName = "content.txt";
         static async Task Main(string[] args)
-           => await new HttpServer(routes => routes
-               .MapGet("/", new TextResponse("Hello from the server!"))
-               .MapGet("/Cats", new HtmlResponse("Hello from the cats!"))
-               .MapGet("/Redirect", new RedirectResponse("https://softuni.org/"))
-               .MapGet("/Html", new HtmlResponse(HtmlForm))
-               .MapPost("/Html", new TextResponse("",StartUp.AddFormDataAction)))
-           .Start();
+        {
+            await DownloadSitesAsTextFile(FileName, new string[] { "https://judge.softuni.org/", "https://softuni.org/" });
+
+            var server = new HttpServer(routes => routes
+              .MapGet("/", new TextResponse("Hello from the server!"))
+              .MapGet("/Cats", new HtmlResponse("Hello from the cats!"))
+              .MapGet("/Redirect", new RedirectResponse("https://softuni.org/"))
+              .MapGet("/Html", new HtmlResponse(HtmlForm))
+              .MapPost("/Html", new TextResponse("", StartUp.AddFormDataAction))
+              .MapGet("/Content", new HtmlResponse(DownloadForm))
+              .MapPost("/Content", new TextFileResponse(FileName)));
+
+            await server.Start();
+        }
+           
 
         private static void AddFormDataAction(Request request, Response response)
         {
@@ -30,6 +42,31 @@
                 response.Body += Environment.NewLine;
             }
         }
+        private static async Task<string> DownloadWebSiteContent(string url)
+        {
+            var client = new HttpClient();
 
+            using (client)
+            {
+                var response = await client.GetAsync(url);
+                var content = await response.Content.ReadAsStringAsync();
+                return content.Substring(0, 2000);
+            }
+        }
+        private static async Task DownloadSitesAsTextFile(string fileName, string[] urls)
+        {
+            var downloads = new List<Task<string>>();
+
+            foreach (var url in urls)
+            {
+                downloads.Add(DownloadWebSiteContent(url));
+            }
+
+            var responses = await Task.WhenAll(downloads);
+
+            var joinedResponses = string.Join(Environment.NewLine + new String('-', 50), responses);
+
+            await File.WriteAllTextAsync(fileName, joinedResponses);
+        }
     }
 }
--- BasicWebServer.Server/Http/Header.cs
+++ BasicWebServer.Server/Http/Header.cs
@@ -6,6 +6,7 @@ namespace BasicWebServer.Server.Http
     {
         public const string ContentType = "Content-Type";
         public const string ContentLength = "Content-Length";
+        public const string ContentDisposition = "Content-Disposition";
         public const string Date = "Date";
         public const string Server = "Server";
         public const string Location = "Location";
