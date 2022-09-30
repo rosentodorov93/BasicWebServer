b647fb25d8de89e46e328357932349313072ad82
Rosen Todorov <rosen.vt55@gmail.com>
2022-09-30 12:54:28 +0300

    Added Httpmethod Attributes

---
+++ BasicWebServer.Server/Attributes/HttpMethodAttribute.cs
@@ -0,0 +1,18 @@
+﻿using BasicWebServer.Server.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicWebServer.Server.Attributes
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    public abstract class HttpMethodAttribute : Attribute
+    {
+        protected HttpMethodAttribute(Method httpMethod)
+            => this.HttpMethod = httpMethod;
+
+        public Method HttpMethod { get;}
+    }
+}
